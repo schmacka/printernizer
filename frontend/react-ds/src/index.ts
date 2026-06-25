f5071826505202275282f698186b7f5e8091b2f6
Sebastian <sebastian@seubertonline.de>
2026-06-25 22:32:00 +0200

    feat: add Button and Card core components

--- frontend/react-ds/src/index.ts
+++ frontend/react-ds/src/index.ts
@@ -1,2 +1,8 @@
 export { PrinternizerProvider } from './PrinternizerProvider';
 export type { PrinternizerProviderProps, PrinternizerTheme } from './PrinternizerProvider';
+
+export { Button } from './core/Button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './core/Button';
+
+export { Card } from './core/Card';
+export type { CardProps } from './core/Card';
